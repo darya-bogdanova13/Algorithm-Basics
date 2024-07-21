3c8fc96a87af457a6bdb1550ab3e7ca3e4dddf33
darya <darya.kolycheva98@yandex.ru>
2024-07-21 18:11:54 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/algorithm_basics/AlgorithmBasicsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.algorithm_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/algorithm_basics/AlgorithmBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.algorithm_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
